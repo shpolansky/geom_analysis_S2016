cc853941b0206fe22e13363bc634a540fb5f8f8d
shpolansky <seth.h.polansky@email.shc.edu>
2016-06-17 13:12:51 -0500

    Fixed the descriptions of the functions

--- geom_mod/shp_geom_mod.py
+++ geom_mod/shp_geom_mod.py
@@ -7,16 +7,18 @@ import math
 import numpy as np
 
 
-#part 1 reading the file
 
 def readgeom(filename):
     """Returns a dictionary of indexed elements with xyz coordinates
 
     Parameters:
-            an xyz file entered as a string
+            string filename -- name of .xyz file containing geometry to be read
 
     Return:
-            a dictionary of indexed elements with xyz coordinates"""
+            dict coord_dict -- dictionary containing all atoms and xyz coordinates.
+            Keys are in the format "SymbolNumber", where Symbol is the element symbol
+            for the atom, and values are lists of the Cartesian coordinates, as floats
+            in [x, y, z] format."""
     coord_dict = {}
     with open(filename, 'r') as f:
         n_atoms = int(f.readline())
@@ -33,19 +35,18 @@ def readgeom(filename):
             atom_list[0] = atom_list[0] + str(Occ)
             symbol_list.append(atom_list[0])
             coord_dict[symbol_list[-1]] = coord_list[-1]  
-    return(coord_dict{})
+    return(coord_dict)
 
 
-#part 2 finding distances
 
 def distance(m, n):
     """Returns the distance between two atomic coordinates
 
     Parameters:
-            two lists of xyz coordinates
+            list m and n -- contains coordinates for atom M, N as floats in [x, y, z] format
    
     Return:
-            the distance value between the coordinates"""
+            the Cartesian distance value between the coordinates"""
     zdiff = m[2] - n[2]
     ydiff = m[1] - n[1]
     xdiff = m[0] - n[0]
@@ -55,26 +56,29 @@ def alldistances(dictionary):
     """Returns all of the distances from a dictionary of coordinates
 
     Parameters:
-            takes a dictionary of indexed atoms and their coordinates
+            dict dictionary -- a dictionary containing keys that are atomic
+            symbols and values that are Cartesian coordinates as floats in [x, y, z] format
 
     Return:
-            a dictionary of the atoms and their distances"""
+            dict distances -- dictionary of the atoms and their distances.
+            The keys are the atomic symbols of the two atoms "Symbol1", "Symbol2"
+            and the values are the distance values between the atoms."""
     distances = {}
     for m, n in itertools.combinations(dictionary.keys(), 2):   
         distances[m, n] = distance(dictionary[m], dictionary[n])
-    return(distances{})
+    return(distances)
 
-#part 3 this is to calculate the angles between the atoms
 
 
 def angle(m, n, s):
     """Returns the angle between three coordinates
 
     Parameters:
-            three lists of atomic xyz coordinates
+            list m, n, s -- contains coordinates for atom M, N, and S
+            as floats in [x, y, z] format
    
     Return:
-            the value of the angle between the coordinates"""
+            the Cartesian value of the angle between the coordinates"""
     list_m = np.array(m)
     list_n = np.array(n)
     list_s = np.array(s)
@@ -88,26 +92,30 @@ def allangles(dictionary):
     """Returns a dictionary of all the angles between the atoms
 
     Parameters:
-            a dictionary of indexed atoms and their xyz coordinates
+            dict dictionary -- dictionary of atoms and their Cartesian coordinates.
+            The keys are element symbols, and the values are lists of Cartesian coordinates
+            as floats in [x, y, z] format.
 
     Return:
-            a dictionary of the atoms and the angles between them"""
+            dict angles -- dictionary of the atoms and the angles between them. The keys
+            are the atomic symbols in "Symbol1", "Symbol2", "Symbol3" format, and the values
+            are the values of the angles between the atoms."""
     angles = {}
     for m, n, s in itertools.permutations(dictionary.keys(), 3):
         angles[m, n, s] = angle(dictionary[m], dictionary[n], dictionary[s])
-    return(angles{})  
+    return(angles)  
 
-#part A 1 finding the out of plane angles
 
 
 def outofplaneangle(m, n, s, t):
     """Returns the angle coming out of the plane for four atoms
 
     Parameters:
-            four lists of atomic xyz coordinates
+            list m, n, s, t -- contains coordinates of atom M, N, S, and T in Cartesian
+            coordinates as floats in [x, y, z] format
 
     Return:
-            the value of the out of plane angle"""
+            the Cartesian value of the out of plane angle"""
     m_list = np.array(m)
     n_list = np.array(n)
     s_list = np.array(s)
@@ -125,14 +133,18 @@ def alloutofplaneangles(dictionary):
     """Returns all of the out of plane angles for a set of atoms
 
     Parameters:
-            a dictionary of indexed atomic symbols and their xyz coordinates
+            dict dictionary -- dictionary of atoms and their Cartesian coordinates. The keys
+            are the atomic symbols, and the values are lists of Cartesian coordinates as floats in 
+            [x, y, z] format.
 
     Return:
-            a dictionary of the atoms and the out of plane angle for each set"""
+            dict outofplaneangles -- dictionary of atoms and the out of plane angles between them.
+            The keys are the atomic symbols in "Symbol1", "Symbol2", "Symbol3", "Symbol4", and the
+            values are the values of the out of plane angles."""
     outofplaneangles = {}
     for m, n, s, t in itertools.permutations(dictionary.keys(), 4):
         outofplaneangles[m, n, s, t] = outofplaneangle(dictionary[m], dictionary[n], dictionary[s], dictionary[t])
-    return(outofplaneangles{})
+    return(outofplaneangles)
 
 
 
